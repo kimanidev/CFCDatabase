f15dbdbaa80f3e56f760fea62a88d0ef5b54dd67
Gediminas Bukauskas <gbukauskas@gmail.com>
2012-02-16 13:15:46 +0200

    Bug fixing release.
    Signed-off-by: Gediminas Bukauskas <gbukauskas@gmail.com>

--- CfCServiceTester/CustomControls/DatabaseBackupContent.ascx.cs
+++ CfCServiceTester/CustomControls/DatabaseBackupContent.ascx.cs
@@ -11,15 +11,19 @@ namespace CfCServiceTester.CustomControls
 {
     public partial class DatabaseBackupContent : System.Web.UI.UserControl
     {
+        private bool emptyFileTemplate;
+
         protected void Page_Load(object sender, EventArgs e)
         {
             this.txtBackupDirectory.Text = (string)WebConfigurationManager.AppSettings["BackupDirectory"];
             this.hdnKillUserProcedure.Value = (string)WebConfigurationManager.AppSettings["KillUserProcedure"];
+            var fileTemplate = (string)WebConfigurationManager.AppSettings["BackupFileTemplate"];
+            this.emptyFileTemplate = !String.IsNullOrEmpty(fileTemplate) && String.Compare(fileTemplate, "None", true) == 0;
         }
 
         public void SetDefaultFileName()
         {
-            if (String.IsNullOrEmpty(this.txtBackupFileName.Text))
+            if (!this.emptyFileTemplate && String.IsNullOrEmpty(this.txtBackupFileName.Text))
             {
                 this.txtBackupFileName.Text = String.Format("{0}_{1}_{2}.bak",
                         CfcWebService.SqlServerName, CfcWebService.DatabaseName, DateTime.Now.ToString("yyyyMMdd_HHmm"));
